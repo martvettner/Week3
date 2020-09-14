e069a4afdeada76febbd7c4b1e746cb721d7fb70
Opilane TTHK <opilane@tthk.ee>
2020-09-14 14:17:49 +0300

    tekst

---
+++ LoginFormRefactored/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoginFormRefactored
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Week3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Week3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Login;
+            string Password;
+            Console.WriteLine("Login: ");
+            Login = Console.ReadLine();
+            Console.WriteLine("Password: ");
+            Password = Console.ReadLine();
+
+            if ((Login == "admin") && (Password == "pass1234"))
+            {
+                Console.WriteLine("Welcome!");
+            }else if ((Login == "admin") && (Password != "pass1234"))
+            {
+                Console.WriteLine("your password is incorrect");
+            }else if ((Login != "admin") && ( Password == "pass1234"))
+            {
+                Console.WriteLine("the login is incorrect");
+            }else
+            {
+                Console.WriteLine("you shall not pass");
+            }
+        }
+    }
+}
